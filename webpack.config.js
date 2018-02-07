96141f482da3dbf782e26d77dcceb54242499d6b
Thomas E Cowern <thomas.e.cowern@gmail.com>
2018-02-07 15:11:26 -0500

    fix for webpack issue

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,17 @@
     // entry -> output
 
     const path = require('path');
+    const webpack = require('webpack');
     const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
+    process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+    if (process.env.NODE_ENV === 'test') {
+        require('dotenv').config({ path: '.env.test' });
+    } else if (process.env.NODE_ENV === 'development') {
+        require('dotenv').config({ path: '.env.development' });
+    }
+    
     console.log(path.join(__dirname, 'public'));
 
     module.exports = (env) => {
@@ -15,7 +24,7 @@
         return {
         entry: './src/app.js',
         output: {
-            path: path.join(__dirname, 'public'),
+            path: path.join(__dirname, 'Public', 'dist'),
             filename: 'bundle.js'
         },
         module: {
@@ -44,12 +53,21 @@
         }]
         },
         plugins: [
-            CSSExtract
+            CSSExtract,
+            new webpack.DefinePlugin({
+                'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
+                'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
+                'process.env.FIREBASE_DATABASE_URL': JSON.stringify(process.env.FIREBASE_DATABASE_URL),
+                'process.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID),
+                'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
+                'process.env.FIREBASE_MANAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MANAGING_SENDER_ID)
+            })
         ],
         devtool: isProduction ? 'source-map' : 'inline-source-map',
         devServer: {
-            contentBase: path.join(__dirname, 'public'),
-            historyApiFallback: true
+            contentBase: path.join(__dirname, 'Public'),
+            historyApiFallback: true,
+            publicPath: '/dist/'
         }
         }
     };
--- webpackold.config.js
+++ webpackold.config.js
@@ -1,17 +1,8 @@
     // entry -> output
 
     const path = require('path');
-    const webpack = require('webpack');
     const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-    process.env.NODE_ENV = process.env.NODE_ENV || 'development';
-
-    if (process.env.NODE_ENV === 'test') {
-        require('dotenv').config({ path: '.env.test' });
-    } else if (process.env.NODE_ENV === 'development') {
-        require('dotenv').config({ path: '.env.development' });
-    }
-    
     console.log(path.join(__dirname, 'public'));
 
     module.exports = (env) => {
@@ -24,7 +15,7 @@
         return {
         entry: './src/app.js',
         output: {
-            path: path.join(__dirname, 'Public', 'dist'),
+            path: path.join(__dirname, 'public'),
             filename: 'bundle.js'
         },
         module: {
@@ -53,21 +44,12 @@
         }]
         },
         plugins: [
-            CSSExtract,
-            new webpack.DefinePlugin({
-                'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
-                'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
-                'process.env.FIREBASE_DATABASE_URL': JSON.stringify(process.env.FIREBASE_DATABASE_URL),
-                'process.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID),
-                'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
-                'process.env.FIREBASE_MANAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MANAGING_SENDER_ID)
-            })
+            CSSExtract
         ],
         devtool: isProduction ? 'source-map' : 'inline-source-map',
         devServer: {
-            contentBase: path.join(__dirname, 'Public'),
-            historyApiFallback: true,
-            publicPath: '/dist/'
+            contentBase: path.join(__dirname, 'public'),
+            historyApiFallback: true
         }
         }
     };
