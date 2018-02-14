58f85b30937bc8fd70fe90f01b9ecc4eaf9c63c1
Thomas E Cowern <thomas.e.cowern@gmail.com>
2018-02-14 11:12:02 -0500

    added update functionality

--- src/components/EditExpense.js
+++ src/components/EditExpense.js
@@ -4,13 +4,13 @@ import {
 } from 'react-redux';
 import ExpenseForm from './ExpenseForm';
 import {
-    editExpense,
+    startEditExpense,
     startRemoveExpense
 } from '../actions/expenses';
 
 export class EditExpensePage extends React.Component {
     onSubmit = (expense) => {
-        this.props.editExpense(this.props.expense.id, expense);
+        this.props.startEditExpense(this.props.expense.id, expense);
         this.props.history.push('/');
     };
 
@@ -39,7 +39,7 @@ const mapStateToProps = (state, props) => ({
 });
 
 const mapDispatchToProps = (dispatch, props) => ({
-    editExpense: (id, expense) => dispatch(editExpense(id, expense)),
+    startEditExpense: (id, expense) => dispatch(startEditExpense(id, expense)),
     startRemoveExpense: (data) => dispatch(startRemoveExpense(data))
 });
 
